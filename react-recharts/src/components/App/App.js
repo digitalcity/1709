fe8ab7bbe1ec18c2e86770c7c5feff989f65adf5
Sunny-zz <1348369672@qq.com>
2017-12-20 16:06:58 +0800

    1

--- react-recharts/src/components/App/App.js
+++ react-recharts/src/components/App/App.js
@@ -7,10 +7,20 @@ class App extends React.Component {
     this.state={
       items:[]
     }
-    this.handle= this.handle.bind(this)
+    this.handle = this.handle.bind(this)
   }
-  handle(){
-    console.log(1)
+  handle(item){
+    if(!this.state.items.find((t)=>t.name===item.name)){
+      this.setState({
+        items:[
+          ...this.state.items,
+          {
+            value:item.value,
+            name:item.name
+          }
+        ]
+      })
+    }
   }
   render () {
     const data01 = [
@@ -24,10 +34,16 @@ class App extends React.Component {
       <div>
         <PieChart width={400} height={300} style={{margin: '0 auto',border:'2px solid #ccc'}}>
           <Pie dataKey="value"  data={data01} cx={200} cy={150}  outerRadius={80} fill="#8884d8" label >
-            {data01.map((item,index) => <Cell key={index} fill={COLORS[index]} />)}
+            {data01.map((item,index) => <Cell key={index} fill={COLORS[index]} onClick={()=>{this.handle(data01[index])}}
+            />)}
           </Pie>
-          <Tooltip/>
+          <Tooltip />
          </PieChart>
+         <div>
+          {
+            this.state.items.map((item,index)=><p key={index}>{item.name}:{item.value}</p>)
+          }
+         </div>
       </div>
     )
   }
